fdf535ad18e9eee0d922c2d6ae4597d9a16be442
tgliu0406 <tiangeliu0528@gmail.com>
2024-10-10 12:04:19 -0700

    test invariance on funnel

--- test/test_Explorers.jl
+++ test/test_Explorers.jl
@@ -1,4 +1,7 @@
 @testset "Invariance tests" begin
+    rng = SplittableRandom(1)
+    step_selectors = (autoRWMH.MHNonAdaptiveSelector(), autoRWMH.MHSelectorInverted())
+    step_jitters = (autoRWMH.StepJitter(dist=Dirac(0)), autoRWMH.StepJitter(dist=Normal(0.0, 0.5)))
     # cannot use the one in Pigeons because conditioning does not work when the observation is an argument
     DynamicPPL.@model function product_of_probs(n_trials)
         p1 ~ Uniform()
@@ -6,22 +9,23 @@
         n_successes ~ Binomial(n_trials, p1*p2)
         return n_successes
     end
-
-    model = product_of_probs(100)
-    target = TuringLogPotential(model)
-    rng = SplittableRandom(1)
-
-    @testset "SimpleRWMH" begin
-        step_selectors = (autoRWMH.MHSelector(), autoRWMH.MHSelectorLegacy(), autoRWMH.MHSelectorInverted())
-        step_jitters = (autoRWMH.StepJitter(dist=Dirac(0)), autoRWMH.StepJitter(dist=Normal(0.0, 0.5)))
+    @model function funnel()
+        y ~ Normal(0, 3)
+        x ~ Normal(0, exp(y/2))
+        z ~ Bernoulli()
+    end
+    targets = (TuringLogPotential(product_of_probs(100)), Pigeons.toy_mvn_target(10), TuringLogPotential(funnel()))
+    kwargs = ( (;condition_on=(:n_successes,)), (;), (;condition_on=(:z,)) )
+    @testset "$(nameof(typeof(target)))" for (target, kwarg) in zip(targets, kwargs)
+        @show nameof(typeof(target))
         foreach(Iterators.product(step_selectors, step_jitters)) do (sss, sj)
             explorer = SimpleRWMH(step_size_selector = sss, step_jitter = sj, n_refresh=50)
             @show explorer
-            @test first(Pigeons.invariance_test(target, explorer, rng; condition_on=(:n_successes,)))
-            @test first(Pigeons.invariance_test(toy_mvn_target(10), explorer, rng))
-        end 
+            @test first(Pigeons.invariance_test(target, explorer, rng; kwarg...))
+        end
     end
 
+    target = TuringLogPotential(product_of_probs(100))
     @testset "HitAndRunSlicer" begin
         explorer = HitAndRunSlicer(n_refresh=50)
         @show explorer
